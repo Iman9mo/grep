9500489a3c3630322810a75edec9fc53c807b42c
Lenovo <imanshans@gmail.com>
2024-01-29 08:29:11 +0330

    implement thread poll

--- os.cpp
+++ os.cpp
@@ -17,152 +17,161 @@ using namespace std;
 pthread_mutex_t readWriteMutex = PTHREAD_MUTEX_INITIALIZER;
 set<string> matches;
 
+void *searchInFile(void *filename);
+void processDirectory(const char *dirPath, const string &searchString);
 
-void* searchInFile(void* filename);
-void processDirectory(const char* dirPath, const string& searchString);
+int main(int argc, char *argv[])
+{
+  string searchedString = argv[1];
+  const char *directoryPath = argv[2];
 
+  processDirectory(directoryPath, searchedString);
 
-int main(int argc, char *argv[]) {
-    string searchedString = argv[1];
-    const char* directoryPath = argv[2];
+  for (auto i : matches)
+  {
+    cout << i << endl;
+  }
 
-    processDirectory(directoryPath, searchedString);
-
-    for (auto i : matches) {
-        cout << i << endl;
-    }
-
-    return 0;
+  return 0;
 }
 void *searchInFile(void *params)
 {
-    string* file = (string*)params;
-    ifstream fileStream(*file);
+  string *file = (string *)params;
+  ifstream fileStream(*file);
 
-    if (!fileStream) return NULL;
-    
-    string line;
-    int lineNum = 1;
-    string searchString = file[1];
+  if (!fileStream)
+    return NULL;
 
-    while (getline(fileStream, line))
+  string line;
+  int lineNum = 1;
+  string searchString = file[1];
+
+  while (getline(fileStream, line))
+  {
+    int lastFoundedIndex = 0;
+    while (true)
     {
-        int lastFoundedIndex = 0;
-        while (true)
-        {
-            size_t foundedIndex = line.find(searchString);
+      size_t foundedIndex = line.find(searchString);
 
-            if(foundedIndex == string::npos) break;
+      if (foundedIndex == string::npos)
+        break;
 
-            line = line.substr(foundedIndex + 1);
-            lastFoundedIndex += foundedIndex;
+      line = line.substr(foundedIndex + 1);
+      lastFoundedIndex += foundedIndex;
 
-            string result = *file + ":" + to_string(lineNum) + ":" + to_string(lastFoundedIndex);
+      string result = *file + ":" + to_string(lineNum) + ":" + to_string(lastFoundedIndex);
 
-            lastFoundedIndex++;
+      lastFoundedIndex++;
 
-            pthread_mutex_lock(&readWriteMutex);
-            matches.insert(result);
-            pthread_mutex_unlock(&readWriteMutex);
-        }
-
-        lineNum++;
+      pthread_mutex_lock(&readWriteMutex);
+      matches.insert(result);
+      pthread_mutex_unlock(&readWriteMutex);
     }
 
-    fileStream.close();
-    pthread_exit(NULL);
+    lineNum++;
+  }
+
+  fileStream.close();
+  pthread_exit(NULL);
 }
 
-void processDirectory(const char* dirPath, const string& searchString) {
-    struct dirent *entry;
-    DIR *dp = opendir(dirPath);
+void processDirectory(const char *dirPath, const string &searchString)
+{
+  struct dirent *entry;
+  DIR *dp = opendir(dirPath);
 
-    if (dp == NULL) {
-        perror("error in opening directory!");
+  if (dp == NULL)
+  {
+    perror("error in opening directory!");
 
-        return;
+    return;
+  }
+
+  while ((entry = readdir(dp)))
+  {
+    if (entry->d_type == DT_REG)
+    {
+      string filename = string(dirPath) + "/" + entry->d_name;
+      pthread_t tid;
+
+      string params[] = {filename, searchString};
+
+      pthread_create(&tid, NULL, searchInFile, (void *)params);
+      pthread_join(tid, NULL);
     }
+    else if (entry->d_type == DT_DIR)
+    {
+      if (entry->d_name[0] != '.')
+      {
+        string subDirectortPath = string(dirPath) + "/" + entry->d_name;
 
-    while ((entry = readdir(dp))) {
-        if (entry->d_type == DT_REG) {
-            string filename = string(dirPath) + "/" + entry->d_name;
-            pthread_t tid;
+        int lengthPipe[2];
+        int resultPipe[2];
 
-            string params[] = {filename, searchString};
+        pipe(lengthPipe);
+        pipe(resultPipe);
 
-            pthread_create(&tid, NULL, searchInFile, (void*)params);
-            pthread_join(tid, NULL);
-        }
-        else if (entry->d_type == DT_DIR)
+        pid_t pid = fork();
+
+        if (pid < 0)
         {
-            if (entry->d_name[0] != '.') {
-                string subDirectortPath = string(dirPath) + "/" + entry->d_name;
+          perror("fork failed");
 
-                int lengthPipe[2];
-                int resultPipe[2];
-
-                pipe(lengthPipe);
-                pipe(resultPipe);
-
-                pid_t pid = fork();
-
-
-                if (pid < 0) {
-                    perror("fork failed");
-
-                    exit(1);
-                } else if (pid == 0) {
-                    close(lengthPipe[0]);
-                    close(resultPipe[0]);
-
-                    processDirectory(subDirectortPath.c_str(), searchString);
-
-                    string writeBuffer;
-
-                    for (auto i : matches)
-                    {
-                        writeBuffer += i + "&&";
-                    }
-
-                    int bufferSize = strlen(writeBuffer.c_str()) + 1;
-
-                    write(lengthPipe[1], to_string(bufferSize).c_str(), strlen(to_string(bufferSize).c_str()) + 1 );
-                    write(resultPipe[1], writeBuffer.c_str(), bufferSize);
-
-                    close(lengthPipe[1]);
-                    close(resultPipe[1]);
-
-                    exit(0);
-                }
-                else
-                {
-                    close(lengthPipe[1]);
-                    close(resultPipe[1]);
-
-                    int status;
-                    wait(&status);
-
-                    char lengthBuffer[255];
-                    read(lengthPipe[0], lengthBuffer, sizeof(lengthBuffer));
-
-                    char readBuffer[stoi(lengthBuffer)];
-                    read(resultPipe[0], readBuffer, sizeof(readBuffer));
-
-                    char *readPointer = strtok(readBuffer, "&&");
-
-                    pthread_mutex_lock(&readWriteMutex);
-                    while (readPointer != NULL)  
-                    {
-                        matches.insert(readPointer);
-                        readPointer = strtok(NULL, "&&");
-                    } 
-                    pthread_mutex_unlock(&readWriteMutex);
-
-                    close(lengthBuffer[0]);
-                    close(resultPipe[0]);
-                }
-            }
+          exit(1);
         }
+        else if (pid == 0)
+        {
+          close(lengthPipe[0]);
+          close(resultPipe[0]);
+
+          processDirectory(subDirectortPath.c_str(), searchString);
+
+          string writeBuffer;
+
+          for (auto i : matches)
+          {
+            writeBuffer += i + "&&";
+          }
+
+          int bufferSize = strlen(writeBuffer.c_str()) + 1;
+
+          write(lengthPipe[1], to_string(bufferSize).c_str(), strlen(to_string(bufferSize).c_str()) + 1);
+          write(resultPipe[1], writeBuffer.c_str(), bufferSize);
+
+          close(lengthPipe[1]);
+          close(resultPipe[1]);
+
+          exit(0);
+        }
+        else
+        {
+          close(lengthPipe[1]);
+          close(resultPipe[1]);
+
+          int status;
+          wait(&status);
+
+          char lengthBuffer[255];
+          read(lengthPipe[0], lengthBuffer, sizeof(lengthBuffer));
+
+          char readBuffer[stoi(lengthBuffer)];
+          read(resultPipe[0], readBuffer, sizeof(readBuffer));
+
+          char *readPointer = strtok(readBuffer, "&&");
+
+          pthread_mutex_lock(&readWriteMutex);
+          while (readPointer != NULL)
+          {
+            matches.insert(readPointer);
+            readPointer = strtok(NULL, "&&");
+          }
+          pthread_mutex_unlock(&readWriteMutex);
+
+          close(lengthBuffer[0]);
+          close(resultPipe[0]);
+        }
+      }
     }
-    closedir(dp);
+  }
+  closedir(dp);
 }
